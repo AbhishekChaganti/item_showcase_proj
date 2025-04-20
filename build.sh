854c9695378eb9dfc2e70c12cb72960d796e421d
AbhishekChaganti <abhishekchaganti25@gmail.com>
2025-04-20 13:00:34 +0530

    Added folder from local machine

--- build.sh
+++ build.sh
@@ -1,3 +1,14 @@
+#!/usr/bin/env bash
+set -o errexit
+
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+# Create superuser if it doesn't exist
+python manage.py shell << END
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'yourpassword')
+END
