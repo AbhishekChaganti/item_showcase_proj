8d9af200421e76cfc463ee0287a67e72a9978b9d
AbhishekChaganti <abhishekchaganti25@gmail.com>
2025-04-20 13:09:12 +0530

    Added folder from local machine

--- build.sh
+++ build.sh
@@ -10,5 +10,5 @@ python manage.py shell << END
 from django.contrib.auth import get_user_model
 User = get_user_model()
 if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@example.com', 'yourpassword')
+    User.objects.create_superuser('abhishekchaganti', 'abhishekchaganti25@gmail.com', 'Abhi@4206')
 END
