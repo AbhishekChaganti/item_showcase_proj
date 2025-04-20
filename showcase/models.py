dbd4373a5592f7032bf40a74121983c0836e21d0
AbhishekChaganti <abhishekchaganti25@gmail.com>
2025-04-20 16:39:29 +0530

    Enable Cloudinary media storage

--- showcase/models.py
+++ showcase/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 class Item(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='items/')
+    image = CloudinaryField('image')
     url = models.URLField(blank=True, null=True)
     
     def __str__(self):
